cda2d19b90a7a4cfe4d0545b6305ba2dca34bcbc
josuepdc <jospdc@gmail.com>
2020-09-12 13:02:32 -0300

    Criando migrations para criação de tabelas Categories e Transactions

---
+++ src/database/migrations/1599926452011-CreateTransactions.ts
@@ -0,0 +1,61 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateTransactions1599926452011
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'transactions',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'type',
+            type: 'varchar',
+          },
+          {
+            name: 'value',
+            type: 'decimal',
+          },
+          {
+            name: 'category_id',
+            type: 'uuid',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'TransactionCategory',
+            columnNames: ['category_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'categories',
+            onDelete: 'RESTRICT',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('transactions');
+  }
+}
