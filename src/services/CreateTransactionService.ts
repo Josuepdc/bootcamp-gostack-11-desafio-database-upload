d58d9f9a1157175fa1980f0f002a52d69c2fc81e
josuepdc <jospdc@gmail.com>
2020-09-12 13:59:55 -0300

    Implementando rota e serviço para criar Transaction

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,55 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
+import Category from '../models/Category';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  categoryTitle: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    categoryTitle,
+  }: Request): Promise<Transaction> {
+    const categoryRepository = getRepository(Category);
+    const transactionRepository = getRepository(Transaction);
+
+    if (!['income', 'outcome'].includes(type)) {
+      throw new AppError(
+        'Invalid transaction type. Only accept "income" or "outcome".',
+        400,
+      );
+    }
+
+    let category = await categoryRepository.findOne({
+      where: { title: categoryTitle },
+    });
+
+    if (!category) {
+      category = categoryRepository.create({
+        title: categoryTitle,
+      });
+
+      await categoryRepository.save(category);
+    }
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
