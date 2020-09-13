047e2c402b4f89464d77a24e8dc9e9a1fc423286
josuepdc <jospdc@gmail.com>
2020-09-13 11:06:05 -0300

    Validando fundos disponíveis para débito

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,8 +1,10 @@
-import { getRepository } from 'typeorm';
+import { getCustomRepository, getRepository } from 'typeorm';
 import AppError from '../errors/AppError';
 
 import Category from '../models/Category';
 import Transaction from '../models/Transaction';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 interface Request {
   title: string;
@@ -19,13 +21,19 @@ class CreateTransactionService {
     categoryTitle,
   }: Request): Promise<Transaction> {
     const categoryRepository = getRepository(Category);
-    const transactionRepository = getRepository(Transaction);
+    const transactionRepository = getCustomRepository(TransactionsRepository);
 
     if (!['income', 'outcome'].includes(type)) {
       throw new AppError(
         'Invalid transaction type. Only accept "income" or "outcome".',
         400,
       );
+    }
+
+    const balance = await transactionRepository.getBalance();
+
+    if (type === 'outcome' && balance.total - value < 0) {
+      throw new AppError('Insufficient balance.', 400);
     }
 
     let category = await categoryRepository.findOne({
