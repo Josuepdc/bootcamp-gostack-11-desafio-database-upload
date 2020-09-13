0f5d6eacfd587dcbb117b96027c134261864b4c4
josuepdc <jospdc@gmail.com>
2020-09-13 11:01:01 -0300

    Implementando listagem de transactions com balance

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,22 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const balance = transactions.reduce(
+      (p, c) => ({
+        income: p.income + (c.type === 'income' ? c.value : 0),
+        outcome: p.outcome + (c.type === 'outcome' ? c.value : 0),
+        total: p.total + (c.type === 'income' ? c.value : -c.value),
+      }),
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    return balance;
   }
 }
 
